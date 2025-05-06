6af72356df1f4f5185dd079b340013e04725aebf
Maikel van de Lisdonk <maikel@devhelpr.com>
2025-05-06 18:30:21 +0200

    Update wrangler configuration and enhance fetch handler logic
    - Modified `wrangler.jsonc` to include a new environment variable `WRANGLER_ENV`.
    - Refactored `fetch` method in `src/index.ts` to implement CORS handling and proxy requests based on incoming headers.
    - Updated test suite in `test/index.spec.ts` to reflect changes in the worker's functionality and added a placeholder test.

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,72 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+// export default {
+// 	async fetch(request, env, ctx): Promise<Response> {
+// 		return new Response('Hello World!');
+// 	},
+// } satisfies ExportedHandler<Env>;
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env): Promise<Response> {
+		const allowedOrigin = ['https://prompttotorm.ai'];
+		const isDev = env.WRANGLER_ENV === 'dev';
+
+		const origin = request.headers.get('Origin') || 'http://localhost';
+		const clientAuth = request.headers.get('Authorization');
+
+		if (!isDev && !allowedOrigin.find((o) => o.startsWith(origin))) {
+			return new Response('Forbidden: invalid origin', { status: 403 });
+		}
+
+		let headers = {};
+		const originUrl = new URL(origin);
+		//const originHost = `${originUrl.protocol}//${originUrl.hostname}`;
+
+		if (request.method === 'OPTIONS') {
+			return new Response(null, {
+				status: 204,
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Methods': '*',
+					'Access-Control-Allow-Headers': '*',
+				},
+			});
+		}
+
+		headers = {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Headers': '*',
+			'Access-Control-Allow-Methods': '*',
+		};
+
+		const apiUrl = request.headers.get('api-url');
+		const path = request.headers.get('api-path');
+
+		if (!apiUrl || !path) {
+			throw new Error(`Missing x-api-url or x-path ${apiUrl} ${path}`);
+		}
+
+		const proxyRequest = new Request(`${apiUrl}/${path}`, {
+			method: request.method,
+			headers: {
+				Authorization: clientAuth || '',
+				'Content-Type': 'application/json',
+			},
+			body: request.body,
+		});
+
+		try {
+			const response = await fetch(proxyRequest);
+			return new Response(response.body, {
+				status: response.status,
+				headers: {
+					'Content-Type': response.headers.get('Content-Type') || 'application/json',
+					...headers,
+				},
+			});
+		} catch (err) {
+			return new Response('Error connecting to AI Gateway', { status: 502 });
+		}
 	},
 } satisfies ExportedHandler<Env>;
